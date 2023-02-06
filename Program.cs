b34443690bf97e69ab775c589f49e5aff48409cf
petrus <pshai671@gmail.com>
2023-02-06 11:28:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Object_oriented_program_of_Addition;
+class Program {
+    static void Main(string[] args)
+    {
+        Test_data dataClass = new Test_data();
+        int add2 = dataClass.Add(45, 34, 67);
+        int add1 = dataClass.Add(23, 34);
+        Console.WriteLine(add1);
+        Console.WriteLine(add2);
+
+    }
+}
---
+++ Test data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented_program_of_Addition
+{
+    public class Test_data
+    {
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
